6cb71871718a0d3e92fb6aa83dfc08ef1f8ab129
Ellis <el1t@ymail.com>
2014-05-16 00:40:30 -0400

    oh noes!
    -Touched up icon to be very very very close
    -Added camera translation controls in “game”
    
    BUG:
    the rotation is not accurate.
    Take a look at it top-down or look at the bottom: it’s not aligned well.
    Y-rotation works by itself, but X&Z combined with Y skew pretty much
    every single axis of rotation, so it’s off in all directions :\
    Pls fix D:

---
+++ Assets/translate.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+function Update () {
+	if(Input.GetKey(KeyCode.LeftArrow))
+		transform.position.x -= .25;
+	else if(Input.GetKey(KeyCode.RightArrow))
+		transform.position.x += .25;
+	if(Input.GetKey(KeyCode.UpArrow))
+		transform.position.y += .25;
+	else if(Input.GetKey(KeyCode.DownArrow))
+		transform.position.y -= .25;
+}
