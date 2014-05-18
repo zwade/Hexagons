707de6873b3f4985b94f188296ce7da25d72b122
addtheletters <bzhang.2003@gmail.com>
2014-05-17 22:16:22 -0400

    angle notes cuz we're dumb

---
+++ Assets/AngleNotes.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+/***
+
+hexagon side length = (cube side length) * Sqrt(6) / 3
+or 2 / Sqrt(6)
+
+angle between overlapping hex grids:
+180 - 2 * arctan( 1/Sqrt(2) )
+
+
+
+
+***/
+
+
+
+
+function Start () {
+
+}
+
+function Update () {
+
+}
